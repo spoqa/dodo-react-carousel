1c4abaf915e60a6f967c529624feaefe03cf1d96
tirr <tirr@spoqa.com>
2018-01-16 14:44:02 +0900

    Basic carousel stylesheets

--- lib/main.tsx
+++ lib/main.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 
+import { carousel, carouselWindow } from './carousel.css';
+
 export class Carousel extends React.Component {
     render() {
         return (
-            <div>
-                Hello, world!
+            <div className={carouselWindow}>
+                <div className={carousel}>
+                    <div>
+                        Hello, world!
+                    </div>
+                    <div>
+                        Hello, world (2)!
+                    </div>
+                </div>
             </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import { Carousel } from '../lib/main';
+import { container } from './index.css';
 
 const content = document.getElementById('content');
 ReactDOM.render(
-    <Carousel />,
+    <div className={container}>
+        <Carousel />
+    </div>,
     content
 );
